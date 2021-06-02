71da2b281034570a60ac305523c947b44d2f73c4
Shauna Myers <shaunamyers26@gmail.com>
2021-06-02 08:31:21 -0700

    Create recipe and ingredient fetch calls

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,3 +1,11 @@
 const retrieveUserData = () => fetch('http://localhost:3001/api/v1/users')
     .then(response => checkForError(response))
     .catch(error => console.log(`User API Error: ${error.message}`));
+
+const retrieveIngredientData = () => fetch('http://localhost:3001/api/v1/ingredients')
+    .then(response => checkForError(response))
+    .catch(error => console.log(`User API Error: ${error.message}`));
+
+const retrieveRecipeData = () => fetch('http://localhost:3001/api/v1/recipes')
+    .then(response => checkForError(response))
+    .catch(error => console.log(`User API Error: ${error.message}`));
