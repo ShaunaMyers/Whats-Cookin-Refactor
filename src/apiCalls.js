3a244b8f39d8882c4ab38855a5f92c04d3fe0a90
Shauna Myers <shaunamyers26@gmail.com>
2021-06-02 09:24:13 -0700

    Create functions for concise Get and Post requests

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,14 +1,37 @@
-const retrieveUserData = () => fetch('http://localhost:3001/api/v1/users')
-    .then(response => checkForError(response))
-    .catch(error => console.log(`User API Error: ${error.message}`));
 
-const retrieveIngredientData = () => fetch('http://localhost:3001/api/v1/ingredients')
-    .then(response => checkForError(response))
-    .catch(error => console.log(`User API Error: ${error.message}`));
+const retrieveData = (path) => {
+    return fetch(path)
+        .then(response => checkForError(response))
+        .catch(err => console.log(`User API Error: ${err.message}`));
+}
 
-const retrieveRecipeData = () => fetch('http://localhost:3001/api/v1/recipes')
-    .then(response => checkForError(response))
-    .catch(error => console.log(`User API Error: ${error.message}`));
+const updateData = (path, data) => {
+    return fetch(path, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+        .then(response => checkForError(response))
+        .catch(err => console.log(err))
+}
+
+
+
+// const retrieveUserData = () => fetch('http://localhost:3001/api/v1/users')
+//     .then(response => checkForError(response))
+//     .catch(error => console.log(`User API Error: ${error.message}`));
+
+//     retrieveIngredientsData: () => fetch('http://localhost:3001/api/v1/ingredients')
+//         .then(response => this.checkForError(response))
+//         .catch(error => console.log(`User API Error: ${error.message}`));
+
+//     const retrieveRecipeData = () => fetch('http://localhost:3001/api/v1/recipes')
+//         .then(response => this.checkForError(response))
+//         .catch(error => console.log(`User API Error: ${error.message}`));
+
+
 
 const checkForError = (response) => {
     if (!response.ok) {
@@ -18,6 +41,10 @@ const checkForError = (response) => {
     }
 }
 
+
 function getData() {
     return Promise.all([retrieveUserData(), retrieveIngredientsData(), retrieveRecipeData()])
 }
+
+
+export default { retrieveUserData, retrieveIngredientsData, retrieveRecipeData, checkForError, getData }
