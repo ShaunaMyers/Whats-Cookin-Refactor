ed61e239aab9b88471ab694fe63f2fd1a9c0c78d
Shauna Myers <shaunamyers26@gmail.com>
2021-06-02 18:31:57 -0700

    Create sampleUsersData to begin Pantry Class and test

---
+++ src/data/sample-users-data.js
@@ -0,0 +1,120 @@
+const sampleUsers = [
+    {
+        "id": 1,
+        "name": "Saige O'Kon",
+        "pantry": [
+            {
+                "ingredient": 11477,
+                "amount": 1
+            },
+            {
+                "ingredient": 93820,
+                "amount": 1
+            },
+            {
+                "ingredient": 11297,
+                "amount": 3
+            },
+            {
+                "ingredient": 11547,
+                "amount": 5
+            },
+            {
+                "ingredient": 1082047,
+                "amount": 5
+            },
+            {
+                "ingredient": 1032050,
+                "amount": 1
+            },
+            {
+                "ingredient": 20081,
+                "amount": 4
+            },
+            {
+                "ingredient": 11215,
+                "amount": 2
+            },
+            {
+                "ingredient": 10514037,
+                "amount": 2
+            }
+        ]
+    },
+    {
+        "id": 2,
+        "name": "Ephraim Goyette",
+        "pantry": [
+            {
+                "ingredient": 6150,
+                "amount": 5
+            },
+            {
+                "ingredient": 1032009,
+                "amount": 3
+            },
+            {
+                "ingredient": 11979,
+                "amount": 5
+            },
+            {
+                "ingredient": 1082047,
+                "amount": 1
+            },
+            {
+                "ingredient": 1034053,
+                "amount": 4
+            },
+            {
+                "ingredient": 99009,
+                "amount": 4
+            },
+            {
+                "ingredient": 2050,
+                "amount": 1
+            },
+            {
+                "ingredient": 19335,
+                "amount": 2
+            },
+            {
+                "ingredient": 1145,
+                "amount": 1
+            },
+            {
+                "ingredient": 9206,
+                "amount": 1
+            },
+            {
+                "ingredient": 18371,
+                "amount": 3
+            },
+            {
+                "ingredient": 19336,
+                "amount": 4
+            },
+            {
+                "ingredient": 14003,
+                "amount": 4
+            },
+            {
+                "ingredient": 2050,
+                "amount": 1
+            },
+            {
+                "ingredient": 11215,
+                "amount": 3
+            },
+            {
+                "ingredient": 9152,
+                "amount": 1
+            },
+            {
+                "ingredient": 11297,
+                "amount": 5
+            }
+        ]
+    }
+]
+
+export default sampleUsers;
