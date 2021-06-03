2e0d36daaec428782be20997da7f3091e25883f3
Sarah Fitzsimons <sarahfitz2713@gmail.com>
2021-06-02 18:44:35 -0600

    Add tests for Cookbook

--- test/Cookbook-test.js
+++ test/Cookbook-test.js
@@ -13,30 +13,38 @@ describe('Cookbook', () => {
   });
 
   it('should be an instance of Cookbook', () => {
-
+    expect(cookbook).to.be.an.instanceof(Cookbook);
   });
 
-  it('Should store all the recipe data', () => {
-
+  it('should store all the recipe data', () => {
+    expect(cookbook.recipes.length).to.be(31);
   });
 
   it('should store all the ingredient data', () => {
-
+    expect(cookbook.ingredients.length).to.be(63);
   });
 
   it('should filter recipes by one tag', () => {
-
+    expect(cookbook.filterByTags('snack'))
+      .to.deep.equal([recipeData[0]]);
   });
 
   it('should filter recipes by multiple tags', () => {
-
+    expect(cookbook.filterByTags(['snack', 'antipasti']))
+      .to.deep.equal([recipeData[0], recipeData[7]]);
   });
 
   it('should allow the user to search recipes by name', () => {
-
+    expect(cookbook.searchForRecipe('cookie')).to.deep.equal([recipeData[0]]);
   });
 
   it('should allow the user to search recipes by ingredient', () => {
-
+    expect(cookbook.searchForRecipe('apple slices'))
+      .to.deep.equal([recipeData[1]]);
   });
-})
+
+  it(`should be able to search favoriteRecipes,
+              without regard to capitalization`, () => {
+    expect(cookbook.searchForRecipe('PUDDING')).to.deep.equal([recipeData[0]]);
+  });
+});
