2ba36a3858792a3e5af990e03717f69a39a44cdc
Sarah Fitzsimons <sarahfitz2713@gmail.com>
2021-06-03 12:01:38 -0600

    Add code in Cookbook to pass tests

---
+++ src/Cookbook.js
@@ -0,0 +1,34 @@
+import ingredientsData from "./data/ingredient-data";
+
+class Cookbook {
+  constructor(recipeData, ingredientData) {
+    this.recipes = recipeData;
+    this.ingredients = ingredientData;
+  }
+
+  filterByTags(tags) {
+    const newFilterTags = typeof tags === "string" ? [tags] : tags;
+    let filteredRecipes = [];
+    newFilterTags.forEach(tag => {
+      this.recipes.forEach(recipe => {
+        if (recipe.tags.includes(tag)) {
+          filteredRecipes.push(recipe)
+        }
+      });
+    });
+
+    return [...new Set(filteredRecipes)];
+  }
+
+  searchForRecipe(searchText) {
+    const newSearchText = searchText.toLowerCase()
+    return this.recipes.filter(recipe => {
+      return recipe.ingredients.find(ingredient => {
+        return (ingredient.name.toLowerCase().includes(newSearchText)) ||
+       (recipe.name.toLowerCase().includes(newSearchText))
+      });
+    })
+  }
+}
+
+export default Cookbook;
--- test/Cookbook-test.js
+++ test/Cookbook-test.js
@@ -1,8 +1,8 @@
 import {expect} from 'chai';
 
 
-import { recipeData } from '../src/data/testData.js';
-import { ingredientsData } from '../src/testData/ingredients';
+import { recipeData } from '../src/data/recipe-data';
+import { ingredientsData } from '../src/data/ingredient-data';
 import Cookbook from '../src/cookbook.js';
 
 let cookbook;
@@ -17,21 +17,21 @@ describe('Cookbook', () => {
   });
 
   it('should store all the recipe data', () => {
-    expect(cookbook.recipes.length).to.be(31);
+    expect(cookbook.recipes.length).to.equal(8);
   });
 
   it('should store all the ingredient data', () => {
-    expect(cookbook.ingredients.length).to.be(63);
+    expect(cookbook.ingredients.length).to.equal(247);
   });
 
   it('should filter recipes by one tag', () => {
     expect(cookbook.filterByTags('snack'))
-      .to.deep.equal([recipeData[0]]);
+      .to.deep.equal([recipeData[0], recipeData[7]]);
   });
 
   it('should filter recipes by multiple tags', () => {
-    expect(cookbook.filterByTags(['snack', 'antipasti']))
-      .to.deep.equal([recipeData[0], recipeData[7]]);
+    expect(cookbook.filterByTags(['snack', 'sauce']))
+      .to.deep.equal([recipeData[0], recipeData[7], recipeData[2]]);
   });
 
   it('should allow the user to search recipes by name', () => {
@@ -43,7 +43,7 @@ describe('Cookbook', () => {
       .to.deep.equal([recipeData[1]]);
   });
 
-  it(`should be able to search favoriteRecipes,
+  it(`should be able to search recipes,
               without regard to capitalization`, () => {
     expect(cookbook.searchForRecipe('PUDDING')).to.deep.equal([recipeData[0]]);
   });
