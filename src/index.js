d2e30b81ee65d4a8193c033ac8d5d39eff31fb5a
Beth <bethm.meeker@gmail.com>
2021-06-07 07:45:13 -0600

    Adjust padding margin for main area in sass file to fix layout

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@
 // Do not delete or rename this file ********
 
 // An example of how you tell webpack to use a CSS (SCSS) file
-import './css/styles.scss';
-import './css/base.scss';
+import './css/index.scss';
+// import './css/_variables.scss';
+// import './css/_mixins.scss';
+// import './css/base.scss';
+// import './css/styles.scss';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 // import './images/turing-logo.png'
